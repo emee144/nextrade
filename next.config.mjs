a2d260aee049e9cd3d3d624ce9d23cc454532205
MAC <mac@MACs-MacBook-Pro.local>
2026-01-09 11:53:00 +0100

    vercel failed build correction caused by turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,25 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
-/** Fix for `__dirname` in ES modules */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// trigger redeploy
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {}, 
+
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@": path.resolve(__dirname, "src/app"), // Define the '@' alias
+      "@": path.resolve(__dirname, "src/app"),
     };
 
-    // Fix Sequelize's "Critical dependency" warning
     config.module.exprContextCritical = false;
-
     return config;
   },
+
   images: {
-    domains: ['cryptologos.cc'], // Allow images from cryptologos.cc
+    domains: ["cryptologos.cc"],
   },
 };
 
